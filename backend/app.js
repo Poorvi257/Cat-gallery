2fa217763678d7fb7e58a4d3f8c1b074cc45d604
poorvi257s <poorvi257s@gmail.com>
2023-11-14 21:25:52 -0500

    add pictureRoutes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userRoutes = require("./routes/v1/userRoutes");
+const pictureRoutes = require("./routes/v1/pictureRoutes");
 const bodyParser = require("body-parser");
 // Import models and associations
 const db = require("./models");
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/user", userRoutes);
+app.use("/picture", pictureRoutes);
 
 // Export the app instance for testing
 module.exports = app;
