132da5feabc3ab3884eefa3da5d92dbd9c7f07e1
poorvi257s <poorvi257s@gmail.com>
2023-11-14 21:23:16 -0500

    removed picture reference temporarily

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const userRoutes = require("./routes/v1/userRoutes");
-const pictureRoutes = require("./routes/v1/pictureRoutes");
 const bodyParser = require("body-parser");
 // Import models and associations
 const db = require("./models");
@@ -12,7 +11,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/user", userRoutes);
-app.use("/picture", pictureRoutes);
 
 // Export the app instance for testing
 module.exports = app;
